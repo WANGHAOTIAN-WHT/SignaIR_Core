fe6fe8f1dd80b67232529e1e6b9cdef5a6e78a7b
Wanght <wanghaotian92@163.com>
2019-10-09 10:42:25 +0800

    首次导入

---
+++ SignalR_Core_Learn/SignalR_Core_Learn/Controller/CountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_Core_Learn.Controller
+{
+    [Route("api/count")]
+    public class CountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalR_Core_Learn/SignalR_Core_Learn/CountHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR_Core_Learn.Service;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignalR_Core_Learn
+{
+    public class CountHub : Hub
+    {
+        private readonly CountService _countService;
+        public CountHub(CountService countService)
+        {
+            _countService = countService;
+        }
+
+        public async Task GetLatestCount(string random)
+        {
+            int count;
+            do
+            {
+                count = _countService.GetLatestCount();
+                Thread.Sleep(1000);
+
+                await Clients.All.SendAsync("RecelveUpdate", count);
+            } while (count < 10);
+            await Clients.All.SendAsync("Finished");
+        }
+    }
+}
